26a00159376a86267c96529911f9c3ae17b7eb7f
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-02-27 19:18:34 +0530

    simultaneous edit feature

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import Quill from "quill"
 import "quill/dist/quill.snow.css"
 import { io } from 'socket.io-client'
@@ -16,13 +16,45 @@ const TOOLBAR_OPTIONS = [
 ]
 
 export default function TextEditor() {
+  const [socket, setSocket] = useState()
+  const [quill, setQuill] = useState()
+
+
   useEffect(() => {
-    const socket = io("https://localhost:3001")
+    const s = io("http://localhost:3001")
+    setSocket(s)
 
     return () => {
-      socket.disconnect()
+      s.disconnect()
     }
-  })
+  }, [])
+
+  useEffect(() => {
+    if (socket == null || quill == null) return
+
+    const handler =  (delta) => {
+      quill.updateContents(delta)
+    }
+    socket.on("receive-changes", handler)
+
+    return () => {
+      socket.off("receive-changes", handler)
+    }
+  }, [socket, quill])
+
+  useEffect(() => {
+    if (socket == null || quill == null) return
+
+    const handler =  (delta, oldDelta, source) => {
+      if (source !== 'user') return
+      socket.emit("send-changes", delta)
+    }
+    quill.on('text-change', handler)
+
+    return () => {
+      quill.off('text-change', handler)
+    }
+  }, [socket, quill])
 
   const wrapperRef = useCallback(wrapper => {
     if (wrapper==null) return
@@ -30,7 +62,11 @@ export default function TextEditor() {
     wrapper.innerHTML = ""
     const editor = document.createElement("div")
     wrapper.append(editor)
-    new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS } })
+    const q = new Quill(editor, { 
+      theme: "snow",
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    })
+    setQuill(q)
 
   }, [])
 
--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const io = require('socket.io')(3001, {
     cors: {
-        origin: 'https://localhost:3000',
+        origin: 'http://localhost:3000',
         methods: ['GET', 'POST'],
     },
 })
 
 io.on("connection", socket => {
-    console.log("connected")
+    socket.on("send-changes", delta => {
+        // console.log(delta)
+        socket.broadcast.emit("receive-changes", delta)
+    })
 })
