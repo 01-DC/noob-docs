cbf1d3178a04d5024f5ea22d2c17e58a7f82b863
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-03-01 14:29:02 +0530

    create server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
+const { Server } = require('socket.io')
 const Document = require('./Document')
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.write('Hello World!'); //write a response
+    res.end(); //end the response
+  }).listen(process.env.PORT, () => {
+   console.log(`server start at port ${process.env.PORT}`);
+  });
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oeg4g.mongodb.net/noob-docs-db?retryWrites=true&w=majority`)
 
-const io = require('socket.io')(process.env.PORT, {
+const io = require('socket.io')(server, {
     cors: {
         // origin: 'http://localhost:3000',
         origin: 'https://noob-docs.vercel.app/',
